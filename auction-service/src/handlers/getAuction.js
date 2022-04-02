191202d43fb1cb8eb489dffc5ae69d19cecf2b7d
Dmytro Poliuliuk <dpoliuliuk@gmail.com>
2022-04-03 00:07:11 +0300

    Added Get(Scan) handler

---
+++ auction-service/src/handlers/getAuction.js
@@ -0,0 +1,37 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+  let auctions;
+
+  try {
+    let results = await dynamodb
+      .scan({
+        // scan - not the best choice (should be query)
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+      })
+      .promise();
+
+    auctions = results.Items;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = middy(getAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
