6be42cf1b7a7bce3bbb52d2768306ddafb89fc7f
Dmytro Poliuliuk <dpoliuliuk@gmail.com>
2022-04-03 21:55:12 +0300

    Adding validations of query (can be body, params ...)

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,7 +1,8 @@
-import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
-import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
+import validator from "@middy/validator";
+import commonMiddleware from "../lib/commonMiddleware";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -36,4 +37,12 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+);
