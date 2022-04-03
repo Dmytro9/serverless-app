2a0b592ddb9a392a421fbe5991960aa06c4d551b
Dmytro Poliuliuk <dpoliuliuk@gmail.com>
2022-04-03 21:38:48 +0300

    Get Auctions by quering

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -6,14 +6,23 @@ import createError from "http-errors";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status",
+    ExpressionAttributeValues: {
+      ":status": status,
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
   try {
-    let results = await dynamodb
-      .scan({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-      })
-      .promise();
+    let results = await dynamodb.query(params).promise();
 
     auctions = results.Items;
   } catch (error) {
